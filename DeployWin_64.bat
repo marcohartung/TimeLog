90282259bea4ac3931d4683a059b5cde5d637ee8
Marco Hartung <marco@helektronik.de>
2018-09-06 22:24:52 +0200

    added script to crate zip for win deployment

---
+++ DeployWin_64.bat
@@ -0,0 +1,19 @@
+# Create zip file with .exe and all dlls for deployment
+# Marco Hartung 06.09.2018
+
+cd..
+if not exist "build" mkdir build
+if exist "build\TimeLog_Winx64" del /Q /F /S "build\TimeLog_Winx64"
+mkdir build\TimeLog_Winx64
+mkdir build\TimeLog_Winx64\tmp
+
+copy /Y "build-TimeLog-Desktop_Qt_5_11_1_MSVC2017_64bit-Release\release\TimeLog.exe" "build\TimeLog_Winx64\tmp"
+
+cd build\TimeLog_Winx64\tmp
+"c:\Qt\5.11.1\msvc2017_64\bin\windeployqt.exe" "TimeLog.exe"
+cd..
+"c:\Program Files\7-Zip\7z.exe" a TimeLog.zip .\tmp\*
+del /F /Q /S "tmp"
+RD /s /q "tmp"
+
+pause
