c71fb29344e2a71d0f4f8d73309a3839d0675bbf
Marco Hartung <marco@helektronik.de>
2019-10-16 21:09:52 +0200

    added GPLv3 comment to source files and licenze text

--- configdlg.cpp
+++ configdlg.cpp
@@ -1,7 +1,24 @@
+/* TimeLog - Copyright (C) 2019  Marco Hartung
+ *
+ * This file is part of TimeLog.
+ *
+ * TimeLog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * TimeLog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TimeLog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "configdlg.h"
 #include <QFileDialog>
 #include "ui_configdlg.h"
-
 
 ConfigDlg::ConfigDlg(QWidget *parent) :
     QDialog(parent, Qt::WindowSystemMenuHint | Qt::WindowTitleHint ),
